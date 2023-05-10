637e09a1a5ff322ede4602fdcbfd23db74dc960e
Zack Bauer <zack.bauer30@gmail.com>
2023-05-10 08:09:36 -0400

    wee

---
+++ planet-facts-react/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <ul>
+            <l1>Earth
+            </l1>
+            <l1>Earth
+            </l1>
+            <li>Earth
+            </li>
+            <l1>Earth
+            </l1>
+            <l1>Earth
+            </l1>
+            <l1>Earth
+            </l1>
+        </ul>
+    );
+}
+
+export default Header;
