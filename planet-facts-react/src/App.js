4058cc5204bc6f57f3a6d6a5a097ec1dc45a26b5
Zack Bauer <zack.bauer30@gmail.com>
2023-05-10 08:57:52 -0400

    mm

--- planet-facts-react/src/App.js
+++ planet-facts-react/src/App.js
@@ -1,14 +1,11 @@
 
 import './App.css';
-
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        hello, i am going to kms
-        .. jk
-        
-      </header>
+      <Header/>
+     
     </div>
   );
 }
--- planet-facts-react/src/components/Header.js
+++ planet-facts-react/src/components/Header.js
@@ -2,20 +2,29 @@ import React from 'react';
 
 function Header(){
     return (
-        <ul>
-            <l1>Earth
+    <div>
+        <div>THE PLANETS</div>
+        <ul className='navbar'>
+            <l1><a>Mercury</a>
             </l1>
-            <l1>Earth
+            <l1><a>Venus</a>
             </l1>
-            <li>Earth
+            <li><a>Earth</a>
             </li>
-            <l1>Earth
+            <l1><a>Mars</a>
             </l1>
-            <l1>Earth
+            <l1><a>Jupiter</a>
             </l1>
-            <l1>Earth
+            <l1><a>Saturn</a>
+            </l1>
+            <l1><a>Uranus</a>
+            </l1>
+            <l1><a>Neptune</a>
+            </l1>
+            <l1><a>Earth</a>
             </l1>
         </ul>
+    </div>
     );
 }
 
