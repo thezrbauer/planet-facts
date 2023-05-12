3a4a0eceb0dfdb9d8297e68fa40f9059811802d7
Zack Bauer <zack.bauer30@gmail.com>
2023-05-11 21:06:41 -0400

    new

--- planet-facts-react/src/components/Header.js
+++ planet-facts-react/src/components/Header.js
@@ -3,26 +3,24 @@ import React from 'react';
 function Header(){
     return (
     <div>
-        <div>THE PLANETS</div>
-        <ul className='navbar'>
-            <l1><a>Mercury</a>
-            </l1>
-            <l1><a>Venus</a>
-            </l1>
-            <li><a>Earth</a>
+       
+        <ul >
+            <li className='navBar'><a>Neptune</a>
             </li>
-            <l1><a>Mars</a>
-            </l1>
-            <l1><a>Jupiter</a>
-            </l1>
-            <l1><a>Saturn</a>
-            </l1>
-            <l1><a>Uranus</a>
-            </l1>
-            <l1><a>Neptune</a>
-            </l1>
-            <l1><a>Earth</a>
-            </l1>
+            <li className='navBar'><a>Uranus</a>
+            </li>
+            <li className='navBar'><a>Saturn</a>
+            </li>
+            <li className='navBar'><a>Jupiter</a>
+            </li>
+            <li className='navBar'><a>Mars</a>
+            </li>
+            <li className='navBar'><a>Earth</a>
+            </li>
+            <li className='navBar'><a>Venus</a>
+            </li>
+            <li className='navBar'><a>Mercury</a>
+            </li>
         </ul>
     </div>
     );
